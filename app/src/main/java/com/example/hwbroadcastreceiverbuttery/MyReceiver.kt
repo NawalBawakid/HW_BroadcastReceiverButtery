f37dbd44d35a5fb2b55d17654fcff80a3a1fa568
Nawal <nawal.khalid.ruh.k@tuwaiq.edu.sa>
2021-12-02 08:59:52 +0300

    -

--- app/src/main/java/com/example/hwbroadcastreceiverbuttery/MyReceiver.kt
+++ app/src/main/java/com/example/hwbroadcastreceiverbuttery/MyReceiver.kt
@@ -9,7 +9,10 @@ class MyReceiver(val textView: AppCompatTextView) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val batteryPercent = intent?.getIntExtra("level", 0)
-        textView.text = if (batteryPercent >= 0) batteryPercent.toString() else ""
+        //textView.text = if (batteryPercent >= 0) batteryPercent.toString() else ""
+
+        textView.text = if (batteryPercent > 50 && batteryPercent < 90) "Normal Battary"
+        else if (batteryPercent < 50) "Low Battary" else "Charged"
 
     }
 }
